8ef7207beaedc63a6438c78f02c624e33f967ff2
Jimena <jimeblue69@gmail.com>
2022-03-26 11:46:17 +0100

    Fetch data using Axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [verbs, setVerbs] = useState(null);
+  useEffect(() => {
+    axiosGet();
+  }, []); //  Run once on load
+
+  const axiosGet = () => {
+    axios
+      .get(`https://my-german-verb-conjugation-api.herokuapp.com/verbs`)
+      .then((data) => setVerbs(data.data));
+  };
+
+  console.log(verbs);
+
+  if (!verbs) return null; // empty render until we get data
   return (
     <section>
       <h1>Conjugate the following verb:</h1>
