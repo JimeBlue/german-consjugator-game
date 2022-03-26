0e4ba337a5b2616a2679c036324cd6ea30de3a17
Jimena <jimeblue69@gmail.com>
2022-03-26 11:43:17 +0100

    Add headings to Home

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section>
+      <h1>Conjugate the following verb:</h1>
+      <ul>
+        <li>Verb: </li>
+        <li>Person: </li>
+        <li>Tense: </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Home;
