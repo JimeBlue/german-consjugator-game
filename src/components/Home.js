54184fd7e404c1b5c40ede5d83ab705067d1601a
Jimena <jimeblue69@gmail.com>
2022-03-26 12:01:03 +0100

    Grab a pronoum with its correct verb form

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import axios from 'axios';
 
 const Home = () => {
   let verb = '';
+  let pronoum = '';
+  let tense = '';
+  let answer = '';
+
   const [verbs, setVerbs] = useState(null);
+
   useEffect(() => {
     axiosGet();
   }, []); //  Run once on load
@@ -24,14 +29,37 @@ const Home = () => {
 
   verb = randomObject.verb;
 
-  console.log(verb);
+  // Get all the conjugations of the verb to be shown
+
+  const conjugationsOfVerb = randomObject.conjugations;
+
+  // Get a random key of the object conjugationsOfVerb to have a tense to show to user
+
+  const randomKeysConjugations = Object.keys(conjugationsOfVerb);
+  tense =
+    randomKeysConjugations[
+      Math.floor(Math.random() * randomKeysConjugations.length)
+    ];
+
+  // Get a pronoum with its corresponding verb form
+
+  for (let conjugation in conjugationsOfVerb) {
+    if (conjugation === tense) {
+      //Grab a random key value pair to have a pronoum to show to use as well as the correct verb form
+      const randomKeyPair = Object.entries(conjugationsOfVerb[tense]);
+      const randomKeyValuePair =
+        randomKeyPair[Math.floor(Math.random() * randomKeyPair.length)];
+      pronoum = randomKeyValuePair[0];
+      answer = randomKeyValuePair[1];
+    }
+  }
   return (
     <section>
       <h1>Conjugate the following verb:</h1>
       <ul>
-        <li>Verb: </li>
-        <li>Person: </li>
-        <li>Tense: </li>
+        <li>Verb: {verb}</li>
+        <li>Person: {pronoum}</li>
+        <li>Tense: {tense}</li>
       </ul>
     </section>
   );
